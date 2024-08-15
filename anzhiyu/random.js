264583dcf612e99a548dfd8f3fd282ecf9e8d880
resheep <3162422258@qq.com>
2024-08-16 01:41:28 +0800

    Site updated: 2024-08-16 01:41:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/01/高中生物总结/","2020/04/01/hello-world/"];function toRandomPost(){
+var posts=["2020/04/01/hello-world/","2024/08/01/高中生物总结/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
